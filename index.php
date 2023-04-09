d9e651cac9aafeed3f8a12a46d0b859261499b6b
Daniil <127922540+VitaNucleus@users.noreply.github.com>
2023-04-09 10:19:28 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>My first PHP Website</title>
+</head>
+<body>
+	<?php
+		echo "<p>Hello, World!</p>";
+	?>
+	<a href="login.php">Click here to login</a>
+	<a href="register.php">Click here ro register</a> 
+</body>
+</html>
